d62be1c5b9627bbb0f0e89dae6f12366acbcde88
elnaz <elnaz.geravand2004@gmail.com>
2026-06-16 16:09:26 +0330

    Add files via upload

---
+++ nutrition.py
@@ -0,0 +1,21 @@
+Fruits = {
+    "Apple": 130 ,
+    "Avacado": 50 ,
+    "Banana": 60 ,
+    "Cantaloupe": 50 , 
+    "Grapefruit" : 60 , 
+    "Grapes" : 90 ,
+    "hobeydaw melon" : 90 , 
+    "kiwifruit": 50 , 
+    "lemon": 90 ,
+    "lime" : 15 ,
+    "orange": 20 , 
+    "peach": 60 ,
+    "pear": 100 , 
+    "plums": 70, 
+    "strawberries" : 50 ,
+    "sweet": 100 ,
+}
+Fruit = input("fruit: ").title()
+if Fruit in Fruits :
+    print ("calories:" , Fruits[Fruit])
